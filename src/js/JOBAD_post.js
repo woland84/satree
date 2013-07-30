1ae9416acd2c85c86ea8857cbfef8e8e8b32ecab
Tom Wiesing <tkw01536@gmail.com>
2013-07-30 11:14:06 +0200

    Updated JOBAD to newer build, updated test page

--- src/js/JOBAD_post.js
+++ src/js/JOBAD_post.js
@@ -7,38 +7,41 @@ JOBAD.modules.register({
         'title':    'SaTree Module',
         'author':   'Tom Wiesing',
         'description':  'Enables the SATree library on the page. ',
+        'externals': {
+            'css': ["satree.min.css"]
+        }
     },
     config: {
-    	"tree_display": ["list", ["apply", "math"], "apply", ["Tree Display", "Apply Mode", "Math Mode"]],
-    	"summarise_math": ["bool", false, ["Summarize Math"]]
+        "tree_display": ["list", ["apply", "math"], "apply", ["Tree Display", "Apply Mode", "Math Mode"]],
+        "summarise_math": ["bool", false, ["Summarize Math"]]
     },
     configUpdate: function(setting, JOBADInstance){
-    	if(setting == "tree_display"){ //Tree mode
-    		if(this.UserConfig.get("tree_display") == "apply"){
-    			satree.ConfigManager.setRenderMode(satree.MathParser.ModeApply);
-			} else {
-				satree.ConfigManager.setRenderMode(satree.MathParser.ModeMath);
-			}
-    	}
-    	if(setting == "summarise_math"){
-    		satree.ConfigManager.setSummarizedMode(this.UserConfig.get("summarise_math")); 
-    	}
+        if(setting == "tree_display"){ //Tree mode
+            if(this.UserConfig.get("tree_display") == "apply"){
+                satree.ConfigManager.setRenderMode(satree.MathParser.ModeApply);
+            } else {
+                satree.ConfigManager.setRenderMode(satree.MathParser.ModeMath);
+            }
+        }
+        if(setting == "summarise_math"){
+            satree.ConfigManager.setSummarizedMode(this.UserConfig.get("summarise_math")); 
+        }
     },
     init: function(JOBADInstance){
-    	var manager = new satree.Manager(JOBADInstance.element);
+        var manager = new satree.Manager(JOBADInstance.element);
         this.localStore.set("manager", manager); //Store the manager in the local Store for this module
     },
     contextMenuEntries: function(target){
-    	var target = target.closest("math");
-    	if(target.length > 0){
-    		var man = this.localStore.get("manager"); 
+        var target = target.closest("math");
+        if(target.length > 0){
+            var man = this.localStore.get("manager"); 
 
-    		return {
-    			"SATree": function(){
-    					man._listenerFunction.call(target.get(0), man); 
-    			}
-    		};
-    	}
+            return {
+                "SATree": function(){
+                        man._listenerFunction.call(target.get(0), man); 
+                }
+            };
+        }
     }
 });
 
